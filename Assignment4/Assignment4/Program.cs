ec82803ff92aeb379eed198ed8df02783e2092b2
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-03-10 21:24:08 +0200

    adding sherlock.txt

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Assignment4;
 
 var fileParser = new FileParser();
-var frequencyTable= fileParser.GetFrequencyTable("C:\\C# projects\\assignment-four-liesnikov-havva\\sherlock.txt");
+var frequencyTable= fileParser.GetFrequencyTable("C:\\C# projects\\assignment4_havva_liesnikov\\sherlock.txt");
 foreach (var varieble in frequencyTable)
 {
     Console.WriteLine($"{varieble.Key}, {varieble.Value}");

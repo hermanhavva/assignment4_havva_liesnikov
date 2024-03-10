4eb5a94f04f6f8ed77351c81312fa93ebdb1970c
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-03-10 21:21:54 +0200

    getting started

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Assignment4;
 
-Console.WriteLine("Hello, World!");
+var fileParser = new FileParser();
+var frequencyTable= fileParser.GetFrequencyTable("C:\\C# projects\\assignment-four-liesnikov-havva\\sherlock.txt");
+foreach (var varieble in frequencyTable)
+{
+    Console.WriteLine($"{varieble.Key}, {varieble.Value}");
+}

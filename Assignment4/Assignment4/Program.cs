5a4c74e8b61355cec9e850bde903895ac332af0b
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-03-10 21:28:20 +0200

    Update Program.cs

--- Assignment4/Assignment4/Program.cs
+++ Assignment4/Assignment4/Program.cs
@@ -2,7 +2,7 @@
 
 var fileParser = new FileParser();
 var frequencyTable= fileParser.GetFrequencyTable("C:\\C# projects\\assignment4_havva_liesnikov\\sherlock.txt");
-foreach (var varieble in frequencyTable)
+foreach (var variable in frequencyTable)
 {
-    Console.WriteLine($"{varieble.Key}, {varieble.Value}");
+    Console.WriteLine($"{variable.Key}, {variable.Value}");
 }

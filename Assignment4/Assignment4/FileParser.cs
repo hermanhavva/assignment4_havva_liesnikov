2ef4a22a19efced03c100e53529e176a82e4a444
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-03-22 16:49:26 +0200

    Update FileParser.cs

--- Assignment4/Assignment4/FileParser.cs
+++ Assignment4/Assignment4/FileParser.cs
@@ -13,7 +13,6 @@ public class FileParser
     public Dictionary<string, int> GetFrequencyTable()
     {
         var frequencyTable = new Dictionary<string, int>();
-        //   var charArray = line.ToCharArray();
         foreach (var symbol in StrText)
             if (!frequencyTable.TryAdd(symbol.ToString(), 1))
                 frequencyTable[symbol.ToString()]++;
